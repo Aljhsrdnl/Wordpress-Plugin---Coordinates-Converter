39301604507cf2c87d18ea2073fc4d5f32733bd9
aljhsrdnl <alejah.sardiniola@wvsu.edu.ph>
2023-11-11 13:03:40 +0800

    feat: added handler function for conversion and css styles

---
+++ js/script.js
@@ -0,0 +1,90 @@
+jQuery(document).ready(function ($) {
+  $("#format").on("change", function () {
+    let format = $(this).val();
+  });
+
+  $("#convert-btn").on("click", function (event) {
+    event.preventDefault();
+    let initial_latitude = $("#latitude").val();
+    let initial_longitude = $("#longitude").val();
+    let format = $("#format").val();
+
+    console.log(format, initial_latitude, initial_longitude);
+    let convertedLatitude = newConvert(format, initial_latitude, true);
+    let convertedLongitude = newConvert(format, initial_longitude, false);
+
+    //display
+    jQuery("#latitude_value").text(convertedLatitude);
+    jQuery("#longitude_value").text(convertedLongitude);
+  });
+});
+
+function newConvert(format, value, isLatitude) {
+  if (format.toLowerCase() === "dd") {
+    // Convert Decimal Degrees (DD) to Degrees, Minutes, and Seconds (DMS)
+    var dms = ddToDMS(value, isLatitude);
+    console.log(dms);
+    return dms;
+  } else if (format.toLowerCase() === "dms") {
+    // Convert Degrees, Minutes, and Seconds (DMS) to Decimal Degrees (DD)
+    var dd = dmsToDD(value);
+    console.log(dd);
+    return dd;
+  } else {
+    return "Invalid format";
+  }
+}
+
+function ddToDMS(value, isLatitude) {
+  // Check if the input is a valid number
+  if (isNaN(value)) {
+    return "Invalid input";
+  }
+
+  // Ensure the input is within the valid range for latitude (-90 to 90) or longitude (-180 to 180)
+  if (Math.abs(value) > 90 && Math.abs(value) > 180) {
+    return "Invalid input range";
+  }
+
+  // Determine the direction (N, S, E, W)
+  var direction = isLatitude
+    ? value >= 0
+      ? "N"
+      : "S"
+    : value >= 0
+    ? "E"
+    : "W";
+
+  const ddRegex = /([-+]?\d+\.\d+)/;
+  const ddMatch = value.match(ddRegex);
+
+  if (ddMatch) {
+    var degrees = Math.floor(value);
+    var minutes = Math.floor((value - degrees) * 60);
+    var seconds = ((value - degrees - minutes / 60) * 3600).toFixed(2);
+
+    return degrees + "° " + minutes + "' " + seconds + '" ' + direction;
+  } else {
+    return "Invalid input.";
+  }
+}
+
+function dmsToDD(value) {
+  //checking
+  const dmsRegex = /(\d+)°(\d+)'([\d.]+)"([NSWE])/;
+  const match = value.match(dmsRegex);
+
+  if (match) {
+    const degrees = parseInt(match[1]);
+    const minutes = parseInt(match[2]);
+    const seconds = parseFloat(match[3]);
+    const direction = match[4];
+
+    const conversion =
+      degrees + minutes / 60 + (seconds / 3600) * (direction === "S" ? -1 : 1);
+
+    return conversion;
+  } else {
+    return "Invalid input.";
+  }
+}
